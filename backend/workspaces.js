f14d64d8904537e6952666d2b7aac0a81f5258e5
Himadri Sankar Chatterjee <himadri.tito13@gmail.com>
2022-02-09 01:16:05 +0530

    added comment to workspace.js

--- backend/workspaces.js
+++ backend/workspaces.js
@@ -1,14 +1,26 @@
+/*
+Schema:
+{
+  name: // Contains the name of the worksace
+  description: //Contains details about the workspace, its requirements and objectives
+}
+*/
+
+// To locally hold the list of all created workspaces
 var workspaces = [];
 
+// Load the 'workspaces' variable with all previusly created workspaces
 const setWorkspaces = (workspaceList) => {
   workspaces = workspaceList;
 };
 
+// Add a new workspace to the list
 const addWorkspace = (workspace) => {
   workspaces.push(workspace);
   return "Addition Successfull";
 };
 
+// Get all the workspaces created
 const getWorkspaces = () => {
   return workspaces;
 };
