182f1b1411b42599ae0044fedc35932bd1b802d0
Himadri Sankar Chatterjee <himadri.tito13@gmail.com>
2022-02-09 01:09:59 +0530

    added comments for index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+// Importing dependencied
 const { Socket } = require("socket.io");
 const redis = require("redis");
 const app = require("express")();
@@ -8,13 +9,16 @@ const io = require("socket.io")(http, {
     methods: ["GET", "POST"],
   },
 });
-require("dotenv").config();
-var workspaceList = [];
-var taskList = [];
 
+require("dotenv").config();
 const { setWorkspaces, addWorkspace, getWorkspaces } = require("./workspaces");
 const { setTasks, addTask, getTasks, getAllTasks } = require("./tasks");
 
+
+var workspaceList = [];
+var taskList = [];
+
+// Defining redis-client to connect to server running in localhsot
 let redisClient = redis.createClient({
   //   host: process.env.REDIS_HOSTNAME,
   //   port: process.env.REDIS_PORT,
@@ -27,6 +31,8 @@ redisClient.on("connect", () => {
   console.log("Redis client Connected");
 });
 
+
+// Bringing back values stored in DB for previously created Workspaces and Tasks
 redisClient.get("workspaces", (error, workspaces) => {
   if (error) console.log(error);
   // console.log(workspaces);
@@ -51,29 +57,40 @@ setTasks(taskList);
 
 console.log("Tasks updated... ");
 
+// Initializing the socket io connection
 io.on("connection", (socket) => {
   socket.on("login", () => {
     console.log("new login");
+    // Returns list of all workspaces within the organization
     io.emit("allWorkspaces", getWorkspaces());
   });
 
   socket.on("openWorkspace", (workspace) => {
     console.log(workspace.name);
+    
+    // Create a new room with the workspace name
     socket.join(workspace.name);
+    // All subsequest emmision happen in the room.
     io.in(workspace.name).emit("allTasks", getTasks(workspace.name));
   });
 
+  // Endpoint for adding new Task
   socket.on("addTask", (task) => {
     console.log(task);
     addTask(task);
     io.in(task.workspaceName).emit("newTask", { task });
+    
+    // Update data in DB
     redisClient.set("tasks", getAllTasks());
   });
 
+  // Endpoint for adding new Workspace
   socket.on("addWorkspace", (workspace) => {
     console.log(getWorkspaces());
     addWorkspace(workspace);
     io.emit("newWorkspace", { workspace });
+    
+    // Store new data into DB
     redisClient.set("workspaces", getWorkspaces());
   });
 
@@ -82,6 +99,7 @@ io.on("connection", (socket) => {
   });
 });
 
+// Starting the server at port 4000
 http.listen(4000, function () {
   console.log("listening on port 4000");
 });
